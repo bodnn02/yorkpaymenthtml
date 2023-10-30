9c7e9153e4c45202a17225eb37520b65cfbbc0f1
bodnn02 <bodnn03@gmail.com>
2023-10-30 17:52:27 +0200

    modified:   css/style.css 	new file:   img/Arrow 1.svg 	new file:   img/amazon.svg 	new file:   img/apple.svg 	new file:   img/booking.svg 	new file:   img/canva.svg 	new file:   img/checkbox-mark.svg 	new file:   img/cloudflare.svg 	new file:   img/copy-btn.svg 	new file:   img/digitalocean.svg 	new file:   img/dropbox.svg 	new file:   img/googleplay.svg 	new file:   img/main-section-bg-1.png 	new file:   img/main-section-bg-2.svg 	new file:   img/main-section-bg-3.svg 	new file:   img/main-section-bg-mobile.png 	new file:   img/onlyfans.svg 	new file:   img/openai.svg 	new file:   img/patreon.svg 	new file:   img/paypal.svg 	new file:   img/steam.svg 	new file:   img/telegram-premium.svg 	new file:   img/trc20.svg 	new file:   img/twitter.svg 	new file:   img/youtube.svg 	modified:   index.html 	modified:   js/main.js 	modified:   user/card-block.html 	modified:   user/card-data.html 	modified:   user/card-deposit.html 	modified:   user/card-issue.html 	modified:   user/profile.html 	modified:   user/transaction-history.html

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ $(".answers-list__expand-btn").on('click', function(e) {
     $(this).closest(".answers-list__item").toggleClass('expanded');
 });
 $(".m-btn").on('click', function(e) {
-    $('.m-btn').addClass('opened')
-    $(".m-overlay").addClass('opened')
-    $(".m-menu").addClass('opened')
+    $('.m-btn').toggleClass('opened')
+});
+$("#m-btn").on('click', function(e) {
+    $(".m-overlay").toggleClass('opened')
+    $(".m-menu").toggleClass('opened')
+});
+$('#profile-m-btn').on('click', function(e) {
+    $(".profile-sidebar").toggleClass('opened')
 });
 $(".m-menu__close-btn").on('click', function(e) {
     $('.m-btn').removeClass('opened')
@@ -25,4 +30,86 @@ $('.overlay .container').on('click', function(e) {
         $('.overlay').removeClass('opened');
         $('.overlay').find('.opened').removeClass('opened');
     }
+});
+$('.select__header').on('click', function(e) {
+    $('.select.opened').removeClass('opened');
+    $(this).closest('.select').toggleClass('opened')
+});
+$('.select-list__item').on('click', function(e) {
+    var select = $(this).closest('.select');
+    var heading = select.find('.select__heading');
+
+    if (select.hasClass('multiple')) {
+        // Переключение выбранного состояния варианта при клике
+        $(this).toggleClass('selected');
+        
+        // Снимаем выбор со всех вариантов, кроме текущего
+        select.find('.select-list__item').not(this).removeClass('selected');
+        
+        // Обновить текст для .select__heading из выбранных вариантов через запятую
+        var selectedItems = select.find('.select-list__item.selected');
+        var selectedText = selectedItems.map(function() {
+            return $(this).text();
+        }).get().join(', ');
+        
+        // Обновить .select__heading
+        heading.text(selectedText);
+
+        // Определить, какой текст нужно показать в зависимости от выбора вариантов
+        if (selectedItems.length === 0) {
+            heading.text("Выберите один или несколько вариантов");
+        } else if (selectedItems.length === 1) {
+            heading.text(selectedText);
+        } else {
+            heading.text("Выберите один или несколько вариантов");
+        }
+    } else {
+        if($(this).hasClass('selected')) {
+            $(this).removeClass('selected')
+            // Определить, какой текст нужно показать в зависимости от выбора вариантов
+            if (select.find('.select-list__item.selected').length === 0) {
+                heading.text("Выберите один вариант");
+            }
+            
+            select.removeClass('opened');
+        } else {
+            // Снимаем выбор со всех вариантов в текущем select
+            select.find('.select-list__item').removeClass('selected');
+            
+            // Добавляем выбор к текущему варианту
+            $(this).addClass('selected');
+            
+            // Устанавливаем текст .select__heading из выбранного варианта
+            heading.text($(this).text());
+            
+            // Закрываем select
+
+            // Определить, какой текст нужно показать в зависимости от выбора вариантов
+            if (select.find('.select-list__item.selected').length === 0) {
+                heading.text("Выберите один вариант");
+            }
+            
+            select.removeClass('opened');
+        }
+    }
+});
+
+$(document).on('click', function(e) {
+    // Проверяем, был ли клик внутри элемента с классом .select
+    if (!$(e.target).closest('.select.opened').length) {
+        // Если не было, то удаляем класс .opened у всех элементов с классом .select
+        $('.select').removeClass('opened');
+    }
+});
+
+
+$('.lang-selector__item').on('click', function(e) {
+    $(this).parent('.lang-selector').children('.lang-selector__item').removeClass('selected')
+    $(this).addClass('selected')
+});
+
+$('.main-section__scroll-btn').on('click', function(e) {
+    $('html, body').animate({
+        scrollTop: $('.popular-services').offset().top
+      }, 1000); // 1000 миллисекунд (1 секунда) для плавной прокрутки
 });
